5c7a52a3f6c0e6d8d68893987f4883bb95e57259
Wahid Khan <wahid.khan74@gmail.com>
2022-05-25 05:36:58 +0000

    added self signed ssl certificate

---
+++ src/test/java/com/simplilearn/webservice/Phase2SpringSslServer05252022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase2SpringSslServer05252022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
